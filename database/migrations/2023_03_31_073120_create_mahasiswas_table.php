54f496d748766d205046d4546b2dbacb214d5cd5
= <=>
2023-04-07 09:54:44 +0700

    first commit

---
+++ database/migrations/2023_03_31_073120_create_mahasiswas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->bigIncrements('Nim');
+            $table->string('Nama');
+            $table->string('Email');
+            $table->string('Tanggal_Lahir');
+            $table->string('Kelas');
+            $table->string('Jurusan');
+            $table->string('No_Handphone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
+
+Route::resource('mahasiswa', MahasiswaController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
